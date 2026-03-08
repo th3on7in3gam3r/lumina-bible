b5ea672c50bc921c61c30d71bffa8f81bd3d717f
Jerless Mahabir <yahweh@Jerlesss-MacBook-Air.local>
2026-03-08 14:49:17 -0400

    fix: allow localhost:3002 CORS origins

--- server/index.ts
+++ server/index.ts
@@ -21,6 +21,9 @@ const ALLOWED_ORIGINS = [
     'https://lumina.biblefunland.com',
     'http://localhost:5173',
     'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'http://localhost:3003',
     'http://localhost:5001'
 ];
 app.use((req, res, next) => {
